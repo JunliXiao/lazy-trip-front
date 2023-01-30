0a5663969c49137d393ccb6d34b7a4c54c80a2d1
JunliXiao <kaifrankwind@gmail.com>
2023-01-30 14:47:12 +0800

    debug

--- page/friend/friend_main.js
+++ page/friend/friend_main.js
@@ -13,11 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function updateUserList() {
     list_users.innerHTML = "";
-    fetch("https://reqres.in/api/users?page=2")
+    fetch("https://spring-boot-rest-1-kbf6hmc46a-de.a.run.app/api/v1/users")
         .then((res) => res.json())
-        .then((data) => data.data.forEach(element => {
+        .then((users) => data.forEach(user => {
             let newItem = document.createElement("li");
-            let text = document.createTextNode(element.email);
+            let text = document.createTextNode(user.fullname);
             newItem.appendChild(text);
             list_users.appendChild(newItem);
         }))
