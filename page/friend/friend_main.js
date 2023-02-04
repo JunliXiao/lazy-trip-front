3c03527b2ea3a2bfa419e0a4a4755eab89193f8a
JunliXiao <kaifrankwind@gmail.com>
2023-02-04 18:53:09 +0800

    建立及使用好友元件

--- page/friend/friend_main.js
+++ page/friend/friend_main.js
@@ -5,22 +5,24 @@
 //     .catch((err) => console.log(err));
 
 const btn_get_all_users = document.getElementById("btn-get-all-users");    
+const btn_show_friends = document.getElementById("btn-show-friends");
 const list_users = document.getElementById("list-users");
+const friends_summary = document.getElementById("friends_summary");
 
 document.addEventListener("DOMContentLoaded", () => {
-    btn_get_all_users.addEventListener("click", updateUserList);
+    btn_show_friends.addEventListener("click", showFriends);
 });
 
-function updateUserList() {
-    list_users.innerHTML = "";
+function showFriends() {
+    friends_summary.innerHTML = '';
+    
     fetch("https://spring-boot-rest-1-kbf6hmc46a-de.a.run.app/api/v1/users")
         .then((res) => res.json())
-        .then((users) => users.forEach(user => {
-            let newItem = document.createElement("li");
-            let text = document.createTextNode(user.fullname);
-            newItem.appendChild(text);
-            list_users.appendChild(newItem);
+        .then((friends) => friends.forEach(fr => {
+            let newItem = document.createElement("friend-component");
+            newItem.setAttribute("fullname", fr.fullname);
+            newItem.setAttribute("username", fr.username);
+            friends_summary.appendChild(newItem);
         }))
         .catch((err) => console.log(err));
-
 }
