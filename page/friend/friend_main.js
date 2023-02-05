25bdda5e70dc6b63e61c8f3b3faf54140b593029
JunliXiao <kaifrankwind@gmail.com>
2023-02-06 01:14:15 +0800

    新增 icon 在好友卡片

--- page/friend/friend_main.js
+++ page/friend/friend_main.js
@@ -7,6 +7,8 @@
 const btn_show_friends = document.getElementById("btn-show-friends");
 const list_users = document.getElementById("list-users");
 const friends_summary = document.getElementById("friends_summary");
+const api_root = "https://spring-boot-rest-1-kbf6hmc46a-de.a.run.app";
+// const api_root = "http://127.0.0.1:8080";
 
 document.addEventListener("DOMContentLoaded", () => {
     btn_show_friends.addEventListener("click", showFriends);
@@ -15,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function showFriends() {
     friends_summary.innerHTML = '';
     
-    fetch("https://spring-boot-rest-1-kbf6hmc46a-de.a.run.app/api/v1/users")
+    fetch(api_root + "/api/v1/users")
         .then((res) => res.json())
         .then((friends) => friends.forEach(fr => {
             let newItem = document.createElement("friend-component");
