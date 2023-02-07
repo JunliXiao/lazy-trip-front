64a1cfbcaf89e63326eccded5267fe4d4384f06e
JunliXiao <kaifrankwind@gmail.com>
2023-02-08 01:33:00 +0800

    略改元件寫法，給好友icon加上放大效果

--- page/friend/fr-components.js
+++ page/friend/fr-components.js
@@ -7,41 +7,49 @@ class Friend extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <div class="card mb-6">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-48x48">
-                <img
-                  src="https://bulma.io/images/placeholders/96x96.png"
-                  alt="Placeholder image"
-                />
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="title is-4">${this.getAttribute("fullname")}</p>
-              <p class="subtitle is-6">@${this.getAttribute("username")}</p>
-            </div>
-            <div class="media-right">
-              <span class="icon is-large is-clickable">
-                <i class="fas fa-lg fa-message"></i>
-              </span>
-              <span class="icon is-large is-clickable">
-                <i class="fas fa-lg fa-people-group"></i>
-              </span>              
-              <span class="icon is-large is-clickable">
-                <i class="fas fa-lg fa-user-xmark"></i>
-              </span>          
-            </div>
+    <style>
+      .icon .fas:hover{
+        font-size: 1.4em;
+        transition: 0.2s ease-out-elastic;
+      } 
+    </style>
+    <div class="card mb-6">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+              />
+            </figure>
           </div>
-
-          <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Phasellus nec iaculis mauris.
+          <div class="media-content">
+            <p class="title is-4">${this.getAttribute("fullname")}</p>
+            <p class="subtitle is-6">@${this.getAttribute("username")}</p>
+          </div>
+          <div class="media-right">
+            <span class="icon is-large is-clickable">
+              <i class="fas fa-lg fa-message"></i>
+            </span>
+            <span class="icon is-large is-clickable">
+              <i class="fas fa-lg fa-people-group"></i>
+            </span>              
+            <span class="icon is-large is-clickable">
+              <i class="fas fa-lg fa-user-xmark"></i>
+            </span>          
           </div>
         </div>
+  
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris.
+        </div>
       </div>
+    </div>
   `;
+
+    this.querySelectorAll("span.icon").forEach(s => s.addEventListener("click", () => console.log("Icon clicked")))
   }
 
 }
