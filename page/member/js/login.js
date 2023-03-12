87dcea6d3a4faa9a2dfcbe6b58ef8c8febbf5aff
Henry <g23821208@gmail.com>
2023-03-12 21:36:09 +0800

    好友按鈕做完，可以根據是否登入，跳轉到登入頁面後再跳轉回上一頁

--- page/member/js/login.js
+++ page/member/js/login.js
@@ -2,6 +2,13 @@ $(function () {
   const account = $("input#input-username");
   const password = $("input#input-password");
   const check = $("div.content-member");
+
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
   $(document).on(
     "keydown",
     "input#input-username, input#input-password",
@@ -24,31 +31,90 @@ $(function () {
   $("button#btn-login").on("click", function () {
     $("p.msg").text();
     if (check.hasClass("-on")) {
-      fetch("login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          account: account.val(),
-          password: password.val(),
-        }),
-      })
-        .then((resp) => {
-          if (resp.redirected) {
-            location.href = resp.url;
-            alert("登入成功");
-          } else {
-            resp.json().then((body) => {
-              $("p.msg").text(body.errorMessage);
-              $("p.msg").css("color", "red");
-              // alert(`errorMsg: ${body.errorMessage}`);
+      if (typeof Storage !== "undefined") {
+        // 檢查 key 是否存在
+        if (sessionStorage.getItem("loc") != null) {
+          // 讀取資料
+          var myData = sessionStorage.getItem("loc");
+          fetch("login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              account: account.val(),
+              password: password.val(),
+            }),
+          })
+            .then((resp) => {
+              if (resp.redirected) {
+                location.href = myData;
+                alert("登入成功");
+              } else {
+                resp.json().then((body) => {
+                  $("p.msg").text(body.errorMessage);
+                  $("p.msg").css("color", "red");
+                  // alert(`errorMsg: ${body.errorMessage}`);
+                });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
             });
-          }
+        } else {
+          fetch("login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              account: account.val(),
+              password: password.val(),
+            }),
+          })
+            .then((resp) => {
+              if (resp.redirected) {
+                location.href = resp.url;
+                alert("登入成功");
+              } else {
+                resp.json().then((body) => {
+                  $("p.msg").text(body.errorMessage);
+                  $("p.msg").css("color", "red");
+                  // alert(`errorMsg: ${body.errorMessage}`);
+                });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      } else {
+        fetch("login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            account: account.val(),
+            password: password.val(),
+          }),
         })
-        .catch((error) => {
-          console.log(error);
-        });
+          .then((resp) => {
+            if (resp.redirected) {
+              location.href = resp.url;
+              alert("登入成功");
+            } else {
+              resp.json().then((body) => {
+                $("p.msg").text(body.errorMessage);
+                $("p.msg").css("color", "red");
+                // alert(`errorMsg: ${body.errorMessage}`);
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     } else {
       // 廠商登入資料
       console.log("company");
